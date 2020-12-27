ff498cb327c985e38d4d2d7593f6af2aeb5a58d1
hitashetty <73528294+hitashetty@users.noreply.github.com>
2020-12-27 14:06:12 +0530

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,18 @@
+class ball {
+    constructor(x,y,radius){
+      var options = {
+      'density' : 1
+      }
+      this.body = Bodies.circle(x,y,radius,options)
+      this.radius = radius;
+      World.add(world,this.body);
+
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        ellipse(0,0,this.radius,this.radius);
+        pop(); 
+    }
+}
